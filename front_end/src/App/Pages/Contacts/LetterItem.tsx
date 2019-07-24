85fec29207038783a4023c2e2e59b8e43a50794a
chao.gu <chao.gu@daoyitong.com>
2019-07-24 09:09:52 +0800

    优化代码

--- front_end/src/App/Pages/Contacts/LetterItem.tsx
+++ front_end/src/App/Pages/Contacts/LetterItem.tsx
@@ -2,54 +2,74 @@ import * as React from 'react'
 import ContactItem from './ContactItem'
 
 export interface letterProps {
-    initial:string
-    list:any[]
+    initial: string
+    list: any[]
     setParentState: any
 }
 
 export default class LetterItem extends React.Component<letterProps, {}> {
-    protected normalGap:number = 12
-    private letterDom:any = React.createRef()
-    private renderContact():any {
+    protected normalGap: number = 12
+    private letterDom: any = React.createRef()
+    private renderContact(): any {
         const list = [...this.props.list]
         return list.map((item, index) => {
-            return <ContactItem normalGap={this.normalGap} id={item.id} name={item.name} index={index} key={item.id} />
+            return (
+                <ContactItem
+                    normalGap={this.normalGap}
+                    id={item.id}
+                    name={item.name}
+                    index={index}
+                    key={item.id}
+                />
+            )
         })
     }
 
-    private initialStyle:object = {
+    private initialStyle: object = {
         color: '#727272',
         display: 'flex',
         alignItems: 'center',
         padding: `${this.normalGap}px`
     }
 
-
-    private handleScroll(e:any):void {
-        const dom:any = this.letterDom.current
+    private handleScroll(e: any): void {
+        const dom: any = this.letterDom.current
         if (dom) {
-            if (e.target.scrollTop > dom.offsetTop && e.target.scrollTop <= dom.offsetTop + dom.offsetHeight) {
-                console.log(this.props.initial, dom.offsetHeight, dom.offsetTop, e.target.scrollTop)
-                this.props.setParentState({currentLetter: this.props.initial})
+            const scrollTop: number = e.target.scrollTop
+            const offsetTop: number = dom.offsetTop
+            const offsetHeight: number = dom.offsetHeight
+            if (
+                scrollTop > offsetTop &&
+                scrollTop <= offsetTop + offsetHeight
+            ) {
+                console.log(
+                    this.props.initial,
+                    offsetHeight,
+                    offsetTop,
+                    scrollTop
+                )
+                this.props.setParentState({ currentLetter: this.props.initial })
             }
             // console.log(this.props.initial, dom.offsetHeight, dom.offsetTop, e.target.scrollTop)
         }
     }
 
-    public componentDidMount():void {
+    public componentDidMount(): void {
         console.log('组件已经渲染完成')
-        window.addEventListener('scroll', (e) => {
-            this.handleScroll(e)
-        }, true)
+        window.addEventListener(
+            'scroll',
+            e => {
+                this.handleScroll(e)
+            },
+            true
+        )
     }
 
     render() {
         return (
             <div ref={this.letterDom}>
                 <h3 style={this.initialStyle}>{this.props.initial}</h3>
-                <ul>
-                    {this.renderContact()}
-                </ul>
+                <ul>{this.renderContact()}</ul>
             </div>
         )
     }
