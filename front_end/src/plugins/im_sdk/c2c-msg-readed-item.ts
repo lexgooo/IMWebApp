4fe838abb3a83f507b280788ec2035f4eb5f1b7b
顾超 <guchao@guchaodeiMac.local>
2019-10-30 15:09:18 +0800

    删除imsdk

--- front_end/src/plugins/im_sdk/c2c-msg-readed-item.ts
+++
@@ -1,8 +0,0 @@
-export default class C2CMsgReadedItemClass {
-    public toAccount:any;
-    public lastedMsgTime:any;
-    constructor (toAccount:any, lastedMsgTime:any) {
-        this.toAccount = toAccount;
-        this.lastedMsgTime = lastedMsgTime;
-    }
-}
